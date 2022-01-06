af67f15b5451c377993f50d8d79ece49db19d8ec
Matej Szendi <matej.szendi@bcom.cz>
2022-01-06 10:50:42 +0100

    ButtonOptions allow add column with class name as type

--- src/Column/ButtonsOptions.php
+++ src/Column/ButtonsOptions.php
@@ -10,35 +10,48 @@ class ButtonsOptions extends AbstractOptions
     /**
      * @var Button[]
      */
-    protected $buttons = [];
+    protected array $buttons = [];
 
     /**
      * @var string
      */
-    protected $separator = '&nbsp;';
+    protected string $separator = '&nbsp;';
 
     /**
      * @param  array|Button[] $buttons
      * @throws Exception
-     * @return ButtonsOptions
+     * @return self
      */
-    public function setButtons(array $buttons)
+    public function setButtons(array $buttons): self
     {
         foreach ($buttons as $button) {
             if ($button instanceof Button) {
                 $btn = $button;
             } else {
-                $type       = isset($button['type']) ? ucfirst(strtolower($button['type'])) : null;
-                $name       = isset($button['name']) ? $button['name'] : null;
-                $options    = isset($button['options']) ? $button['options'] : null;
-                $attributes = isset($button['attributes']) ? $button['attributes'] : null;
-                $conditions = isset($button['conditions']) ? $button['conditions'] : null;
-                $class = 'Lemo\Grid\Column\\' . $type;
+                $type       = $button['type'] ?? null;
+                $name       = $button['name'] ?? null;
+                $options    = $button['options'] ?? null;
+                $attributes = $button['attributes'] ?? null;
+                $conditions = $button['conditions'] ?? null;
 
-                if (!in_array($type, ['Button', 'Buttonlink', 'Route'])) {
-                    throw new Exception('Button type must be Button, ButtonLink or Route');
+                if (false !== strpos($type, 'Lemo\Grid\Column')) {
+                    if (!in_array($type, [Button::class, Buttonlink::class, Route::class])) {
+                        throw new Exception('Button type must be Button, ButtonLink or Route');
+                    }
+
+                    $btn = new $type($name, $options, $attributes, $conditions);
+                } else {
+                    $type = ucfirst(strtolower($type));
+
+                    if (!in_array($type, ['Button', 'Buttonlink', 'Route'])) {
+                        throw new Exception('Button type must be Button, ButtonLink or Route');
+                    }
+
+                    $class = 'Lemo\Grid\Column\\' . $type;
+
+                    $btn = new $class($name, $options, $attributes, $conditions);
                 }
-                $btn = new $class($name, $options, $attributes, $conditions);
+
             }
 
             $this->buttons[] = $btn;
@@ -50,15 +63,15 @@ class ButtonsOptions extends AbstractOptions
     /**
      * @return Button[]
      */
-    public function getButtons()
+    public function getButtons(): array
     {
         return $this->buttons;
     }
 
     /**
-     * @return ButtonsOptions
+     * @return self
      */
-    public function clearButtons()
+    public function clearButtons(): self
     {
         $this->buttons = [];
 
@@ -67,9 +80,9 @@ class ButtonsOptions extends AbstractOptions
 
     /**
      * @param  string $separator
-     * @return ButtonsOptions
+     * @return self
      */
-    public function setSeparator($separator)
+    public function setSeparator(string $separator): self
     {
         $this->separator = $separator;
 
@@ -79,7 +92,7 @@ class ButtonsOptions extends AbstractOptions
     /**
      * @return string
      */
-    public function getSeparator()
+    public function getSeparator(): string
     {
         return $this->separator;
     }
